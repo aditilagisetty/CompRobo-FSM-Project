ee4463d8031e9e515e53a6fa813bd2624570ddee
AkilPugal07 <akilpugalenthi@gmail.com>
2026-09-21 16:00:35 -0400

    Fixed some logic bugs

--- ros_behaviors_fsm/wall_follower.py
+++ ros_behaviors_fsm/wall_follower.py
@@ -15,6 +15,9 @@ class WallFollower(Node):
         self.kp = (
             0.5  # TODO: tune this proportional gain to get good wall-following behavior
         )
+        self.follow_side = (
+            None  # +1 for left wall, -1 for right wall, None for no wall detected
+        )
 
     def process_scan(self, msg):
         # TODO: pick two (or more) laser measurements to estimate the angle
@@ -25,32 +28,70 @@ class WallFollower(Node):
 
         front_dist = msg.ranges[0]
 
+        r45 = msg.ranges[45]
+        r90 = msg.ranges[90]
+        r135 = msg.ranges[135]
+        r225 = msg.ranges[225]
+        r270 = msg.ranges[270]
+        r315 = msg.ranges[315]
+
+        decided_angles = []
+
+        # This checks what wall is close to the neato and sets the follow side accordingly. If both walls are close, it will follow the left wall.
+        r90_valid = math.isfinite(r90) and r90 > 0.0
+        r270_valid = math.isfinite(r270) and r270 > 0.0
+
+        # This checks what wall is close to the neato and sets the follow side accordingly. If both walls are close, it will follow the left wall.
+        if r90_valid and r270_valid:
+            if r90 <= r270:
+                self.follow_side = 1  # Left side is closer
+            else:
+                self.follow_side = -1  # Right side is closer
+        elif r90_valid:
+            self.follow_side = 1  # Only left wall visible
+        elif r270_valid:
+            self.follow_side = -1  # Only right wall visible
+
+        # Assign the angles to be used for wall following based on the follow side
+        if self.follow_side == 1:
+            decided_angles = [r45, r90, r135]
+        else:
+            decided_angles = [r225, r270, r315]
+
         if (
             not math.isnan(front_dist)
             and not math.isinf(front_dist)
             and front_dist > 0.0
         ):
-            # if the front distance is less than equal to 1.0 meters, stop the robot and turn left
+            # If the front distance is less than or equal to 1.0 meters, stop and turn away from the wall
             if front_dist <= 1.0:
                 vel.linear.x = 0.0
-                vel.angular.z = -1.0
+                vel.angular.z = float(
+                    self.follow_side * -0.1
+                )  # Turn left or right based on follow side
                 self.vel_pub.publish(vel)
                 return
 
-        if any(math.isinf(r) or math.isnan(r) for r in msg.ranges[45:136]):
+        if any(
+            math.isinf(r) or math.isnan(r)
+            for r in [decided_angles[0], decided_angles[1], decided_angles[2]]
+        ):
             vel.linear.x = self.forward_speed
-            vel.angular.z = 0.1
+            vel.angular.z = float(self.follow_side * 0.1)
             self.vel_pub.publish(vel)
             return
 
-        error1 = msg.ranges[90] - self.distance_from_wall
-        error_allign = msg.ranges[45] - msg.ranges[135]
+        error1 = decided_angles[1] - self.distance_from_wall
+        error_allign = decided_angles[0] - decided_angles[2]
 
         # Calculation for the error in allignment along the wall
         vel.linear.x = float(self.forward_speed)
-        vel.angular.z = float((self.kp * error1) + (self.kp * error_allign))
+        vel.angular.z = float(
+            self.follow_side * ((self.kp * error1) + (self.kp * error_allign))
+        )
 
         # DEbug print statements
+        print(f"Side: {'LEFT' if self.follow_side == 1 else 'RIGHT'}")
         print(f"Angular Z: {vel.angular.z}")
 
         self.vel_pub.publish(vel)
